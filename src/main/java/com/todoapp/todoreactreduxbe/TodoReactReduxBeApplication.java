602ed294443446b3af635fedd6bc7b7dce292113
HoangVinhNguyen <ngvinh719@gmail.com>
2021-12-19 23:37:26 +0700

    init project

---
+++ src/main/java/com/todoapp/todoreactreduxbe/TodoReactReduxBeApplication.java
@@ -0,0 +1,13 @@
+package com.todoapp.todoreactreduxbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReactReduxBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoReactReduxBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoapp/todoreactreduxbe/TodoReactReduxBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoapp.todoreactreduxbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoReactReduxBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
